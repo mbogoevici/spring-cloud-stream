a17132bf2ecee64a8eba0edc3d3338c4ec3c542b
Marius Bogoevici <mbogoevici@pivotal.io>
2015-07-28 11:30:48 -0400

    Tests

--- spring-cloud-stream-samples/sink/src/test/java/demo/ModuleApplicationTests.java
+++ spring-cloud-stream-samples/sink/src/test/java/demo/ModuleApplicationTests.java
@@ -1,11 +1,20 @@
 package demo;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.stream.adapter.ChannelBindingAdapter;
+import org.springframework.cloud.stream.adapter.ChannelLocator;
+import org.springframework.cloud.stream.adapter.DefaultChannelLocator;
+import org.springframework.cloud.stream.adapter.InputChannelBinding;
 import org.springframework.cloud.stream.annotation.ModuleChannels;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.Sink;
@@ -15,6 +24,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import org.mockito.internal.matchers.NotNull;
 import sink.LogSink;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -30,12 +40,24 @@ public class ModuleApplicationTests {
 	@Autowired
 	private Sink same;
 
+
+	@Autowired
+	private ChannelBindingAdapter locator;
+
+
+	// Not injected
 	@Output(Source.OUTPUT)
 	private MessageChannel output;
 
 	@Test
 	public void contextLoads() {
 		assertNotNull(this.sink.input());
+	}
+
+	@Test
+	public void bindings() {
+		assertThat(locator.getInputChannel("input"), notNullValue(InputChannelBinding.class));
+		assertThat(locator.getInputChannel("input").getRemoteName(), equalTo("testtock"));
 	}
 
 }
