1a911a4149962089fd02269b040d2abb6dbe2c1e
Marius Bogoevici <mbogoevici@pivotal.io>
2016-07-27 21:19:36 -0400

    First round of porting Schema management code

---
+++ spring-cloud-stream-schema/src/main/java/org/springframework/cloud/stream/schema/avro/AvroMessageConverterAutoConfiguration.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.schema.avro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.binder.StringConvertingContentTypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Marius Bogoevici
+ * @author Vinicius Carvalho
+ */
+@Configuration
+@ConditionalOnClass(name = "org.apache.avro.Schema")
+@EnableConfigurationProperties(AvroMessageConverterProperties.class)
+public class AvroMessageConverterAutoConfiguration {
+
+	@Autowired
+	private AvroMessageConverterProperties avroMessageConverterProperties;
+
+	@Bean
+	public AvroSchemaMessageConverter avroSchemaMessageConverter(SchemaRegistryClient schemaRegistryClient) {
+		AvroSchemaMessageConverter avroSchemaMessageConverter = new AvroSchemaMessageConverter(schemaRegistryClient);
+		avroSchemaMessageConverter.setDynamicSchemaGenerationEnabled(
+				this.avroMessageConverterProperties.isDynamicSchemaGenerationEnabled());
+		avroSchemaMessageConverter.setContentTypeResolver(new StringConvertingContentTypeResolver());
+		return avroSchemaMessageConverter;
+	}
+}
---
+++ spring-cloud-stream-schema/src/main/java/org/springframework/cloud/stream/schema/avro/SchemaRegistryClient.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.schema.avro;
+
+import org.apache.avro.Schema;
+
+/**
+ * @author Vinicius Carvalho
+ * @author Marius Bogoevici
+ */
+public interface SchemaRegistryClient {
+
+	/**
+	 * Registers a schema with the remote repository returning the unique identifier associated with this schema.
+	 * version
+	 * @param subject the full name of the schena
+	 * @param schema
+	 * @return a {@link SchemaRegistrationResponse} representing the result of the operation
+	 */
+	SchemaRegistrationResponse register(String subject, Schema schema);
+
+	/**
+	 * Retrieves a schema by its identifier.
+	 * @param id
+	 * @return
+	 */
+	Schema fetch(SchemaReference id);
+
+}
