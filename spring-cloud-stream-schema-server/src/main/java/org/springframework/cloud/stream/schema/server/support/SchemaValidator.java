cc5699e65b5198f0a70f52731f7fde428221010c
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-07-28 20:56:18 -0400

    Fixing checkstyle

--- spring-cloud-stream-schema-server/src/main/java/org/springframework/cloud/stream/schema/server/support/SchemaValidator.java
+++ spring-cloud-stream-schema-server/src/main/java/org/springframework/cloud/stream/schema/server/support/SchemaValidator.java
@@ -1,18 +1,17 @@
 /*
- *  Copyright 2016 original author or authors.
+ * Copyright 2016 the original author or authors.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.springframework.cloud.stream.schema.server.support;
@@ -35,7 +34,7 @@ public interface SchemaValidator {
 	 * @param definition - The textual representation of the schema file
 	 * @return
 	 */
-	public boolean isValid(String definition);
+	boolean isValid(String definition);
 
 	/**
 	 * Checks for compatibility between two schemas @see Compatibility class for types
@@ -44,7 +43,7 @@ public interface SchemaValidator {
 	 * @param other - The textual representation of the other schema to tested
 	 * @return
 	 */
-	public Compatibility compatibilityCheck(String source, String other);
+	Compatibility compatibilityCheck(String source, String other);
 
 	/**
 	 * Return the Schema that is represented by the definition.
@@ -52,8 +51,8 @@ public interface SchemaValidator {
 	 * @param definition Textual representation of the schema
 	 * @return A full Schema object with identifier and subject properties
 	 */
-	public Schema match(List<Schema> schemas, String definition);
+	Schema match(List<Schema> schemas, String definition);
 
-	public String getFormat();
+	String getFormat();
 
 }
